71b9e620c1afacdf39426cf7069ea128ca6a2d1a
Gouri Nandakumar <gouri.nandakumar26@gmail.com>
2018-10-13 14:19:30 +0200

    added dynamic ingredients with changing number of ingredients in burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,22 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {...};
+  // }
+  state =  {
+    ingredients : {
+      cheese : 1,
+      salad : 3,
+      bacon : 2,
+      meat : 1
+    }
+  }
   render() {
     return (
       <Aux>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
