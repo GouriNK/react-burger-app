46982d69965d31e33dbbf2c5087e955e7f916c2d
Gouri Nandakumar <gouri.nandakumar26@gmail.com>
2018-10-19 17:42:23 +0200

    added events on the order summary modal to cancel/continue the order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   }
 
+  purchaseContinueHandler = () => {
+    console.log('You continue');
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -97,7 +101,12 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients}></OrderSummary>
+          <OrderSummary
+          purchaseCanceled = {this.purchaseCancelHandler}
+          purchaseContinued = {this.purchaseContinueHandler}
+          ingredients={this.state.ingredients}
+
+          ></OrderSummary>
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
